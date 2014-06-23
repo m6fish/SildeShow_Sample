2a51a77e95d7ed956c975fa550d22e0b50f2de73
Hao Ping Yu <pumpkinjack@livemail.tw>
2014-06-23 17:11:09 +0800

    add sample3 :full screen , auto & loop scrolling

--- readme.txt
+++ readme.txt
@@ -6,3 +6,4 @@ http://sorgalla.com/jcarousel/
 DIR
   -sample1 : 簡單的套用範例,使用官網的教學製作
   -sample2 : 自動+循環播放,套用範例一
+  -sample2 : 滿版設計+自動+循環撥放(chrome35,firefox29)
---
+++ sample3/jcarousel_setting.js
@@ -0,0 +1,52 @@
+(function($) {
+    $(function() {
+        $('.jcarousel').jcarousel({
+          //循環播放
+          /*
+            -first:過頭回到尾端
+            -last :過尾回到頭端
+            -both :雙向連接頭尾
+            -circular:循環播放
+          */
+          wrap: 'circular',
+        })
+        //自動撥放
+        .jcarouselAutoscroll({            
+            interval: 500,
+            target: '+=1',
+            autostart: true
+        });
+        
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+    });
+})(jQuery);
+
